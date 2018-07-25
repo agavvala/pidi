da62addbec7095bde2da98ed1497c7127e5a0790
tkantamneni <teja.kantamneni@gmail.com>
2018-07-25 12:47:46 -0400

    TestWords basic and constants

--- src/TestWords.js
+++ src/TestWords.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 
+import {DEFAULT_ASSESSMENT_QUESTION_COUNT} from './Wellknown'
+import PidiWebServices from './services/pidi_webservices'
+
+
 class TestWords extends Component {
-    render() {
-        return (
+    pidiService = new PidiWebServices()
+    constructor(props){
+        super(props);
+        //this.state = {currentQuestion: null};
+    }
+
+    componentDidMount() {
+        this.pidiService.fetchTest(DEFAULT_ASSESSMENT_QUESTION_COUNT, this.startTest)
+    }
+
+    startTest = (result) => {
+        this.setState({
+            currentQuestion: result.data[0]
+        });
+    }
+    render(){
+        return(
             <div>
-                Test your knowledge
+                <h2>Test your knowledge</h2>
+                <h4>Number of questions: {DEFAULT_ASSESSMENT_QUESTION_COUNT}</h4>
             </div>
-    );
+        );
     }
 }
 
---
+++ src/Wellknown.js
@@ -0,0 +1,8 @@
+
+export const DEFAULT_ASSESSMENT_QUESTION_COUNT = 10;
+
+export const appDefaults = {
+    assessmentQuestionCount: DEFAULT_ASSESSMENT_QUESTION_COUNT
+}
+
+export default {...appDefaults}
