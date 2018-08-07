6d45b56c8b21122c0b7f0d086c2ef60e99cc8a42
tkantamneni <teja.kantamneni@gmail.com>
2018-08-07 08:01:02 -0400

    aonaware service complete with test case. now it returns an array of word definitions for a given word.

--- src/services/AonawareServices.js
+++ src/services/AonawareServices.js
@@ -1,21 +1,26 @@
-var XMLParser = require('react-xml-parser');
-const axios = require('axios');
-
+const request = require('request');
+const parseString = require('xml2js').parseString;
 import {AONAWARE_ENDPOINT} from '../Wellknown'
 
 class AonawareServices {
-
     fetchWordInformation(word){
-        var endpoint = AONAWARE_ENDPOINT + word;
-        axios.get(endpoint)
-            .then(response => {
-                console.log(response);
-                //var xml = new XMLParser().parseFromString(response);
-                //console.log(xml);
-            })
-            .catch(error => {
-                console.log(error);
+        const endpoint = AONAWARE_ENDPOINT + word;
+        request(endpoint, (err, res, body) => {
+            if (err) { return console.log(err); }
+            //console.log(body);
+            let wordDefinitions = []
+            parseString(body, function (err, result) {
+                // console.log(result)
+                result.WordDefinition.Definitions.forEach(e => {
+                    e.Definition.forEach(ed => {
+                        // console.log(ed.WordDefinition)
+                        wordDefinitions.push(ed.WordDefinition)
+                    })
+                })
             });
+            console.log(wordDefinitions)
+            return wordDefinitions;
+        });
     }
 }
 
